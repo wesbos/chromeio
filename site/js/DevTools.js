8d056ced125ca9cfdf4dfafdf1f82838786aef19
Wes Bos <wesbos@gmail.com>
2011-08-29 22:28:29 -0400

    Initial code to add dynamic colour selection

--- site/js/DevTools.js
+++ site/js/DevTools.js
@@ -2,7 +2,8 @@ $(function() {
 
 	var chromeio = window.chromeio = {};
 		chromeio.styles = {},
-		chromeio.edit = {} 
+		chromeio.edit = {},
+		chromeio.activeStyle;
 
 	// Events
 	
@@ -37,27 +38,58 @@ $(function() {
 		e.stopPropagation();
 		
 		// Match the classes
-	var classy = "watt";
-		var classy = this.className.match(/webkit-[\w-]*/);		
-		console.log(classy + '');
+		var classy = this.className.match(/webkit-[\w-]*/) + '';		
 		
 		// Update the style object
 		if (!chromeio.styles[classy]) {
 			console.log('nothing found for '+ classy);
-			chromeio.styles[classy] = {};
+			chromeio.styles[classy] = {
+				"name" : classy,
+				"color" : "#fff",
+				"font-style" : "italic"
+			};
 		} 
 		else {
-			console.log('found it! we will update your styles');
+			console.log('found it! we will update your styles',chromeio.styles[classy]);
 		}
+		
+		chromeio.activeStyle = chromeio.styles[classy].name; 
+		chromeio.viewStyles(chromeio.styles[classy]);
+		
 		
 
 	});
 	
+	chromeio.viewStyles = function(obj) {
+		html = "<h3>"+ obj.name +"</h3>";
+		for (key in obj) {
+			if (obj.hasOwnProperty(key) && key !== "name") {
+				//console.log(key, obj[key])
+				html += "<hr />";
+				html += key + "- <input type='text' data-cssRule='"+ obj.name +"' class='colorwheel' value='" + obj[key] + "' />";
+			}
+		}
+		$('.editorInner').html(html);
+		startColorPicker(); 
+	}
 	
+	
+	// colorpicker
+	function startColorPicker() {
+		$('.colorwheel').ColorPicker({
+			onSubmit: function(hsb, hex, rgb, el) {
+				$(el).val(hex);
+				$(el).ColorPickerHide();
+			},
+			onBeforeShow: function () {
+				$(this).ColorPickerSetColor(this.value);
+			},
+			onChange: function (hsb, hex, rgb) {
+				$('#elements-content2 .'+chromeio.activeStyle).css('color', '#' + hex);
+			}
+		})
+		.bind('keyup', function(){
+			$(this).ColorPickerSetColor(this.value);
+		});
+	}	
 });
-
-
-
-
-
-
